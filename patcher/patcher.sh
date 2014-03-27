bd73bc9e1025bee9f9cde10c2abad6b6b35d2441
jereksel <jereksel@gmail.com>
2014-03-27 20:58:01 +0100

    Update patcher/patches

--- patcher/patcher.sh
+++ patcher/patcher.sh
@@ -1,46 +1,33 @@
 #!/bin/sh
-echo "MaclawStudio patcher for Ace 2 /4.4/\n";
+echo -e "\e[0;32mNovaFusion patcher for CyanogenMod 11\nStarting..."
 
-echo "Copying AV patch...\n";
-cp av.patch ../../../../frameworks/av
-echo "Copying NATIVE patch...\n";
-cp native.patch ../../../../frameworks/native
-echo "Copying CORE patch...\n";
-cp core.patch ../../../../system/core
-echo "Copying TELEPHONY patch...\n";
-cp telephony.patch ../../../../packages/services/Telephony
-echo "Copying RECOVERY patch...\n";
-cp recovery.patch ../../../../bootable/recovery
-echo "Copying CAMERA patch...\n";
-cp camera.patch ../../../../packages/apps/Camera2
-echo "Copying BIONIC patch...\n";
-cp bionic.patch ../../../../bionic
-cd ../../../../frameworks/av
-echo "Patching AV...\n";
-patch -p1 < av.patch
-rm av.patch
-cd ../native
-echo "Patching NATIVE...\n";
-patch -p1 < native.patch
-rm native.patch
-cd ../../system/core
-echo "Patching CORE...\n";
-patch -p1 < core.patch
-rm core.patch
-cd ../../packages/services/Telephony
-echo "Patching TELEPHONY...\n";
-patch -p1 < telephony.patch
-rm telephony.patch
-cd ../../../bootable/recovery
-echo "Patching RECOVERY...\n";
-patch -p1 < recovery.patch
-rm recovery.patch
-cd ../../packages/apps/Camera2
-echo "Patching CAMERA...\n";
-patch -p1 < camera.patch
-rm camera.patch
-cd ../../../bionic
-echo "Patching BIONIC...\n";
-patch -p1 < bionic.patch
-rm bionic.patch
-echo "Done. Have a nice day ;-)";
+PATCHERDIR=$(dirname $(readlink -f "$0"))
+cd $PATCHERDIR
+cd ../../../..
+BASEDIR=$(pwd)
+
+echo -e "\n\e[0;31mPatching RECOVERY:\e[0;34m"
+cd $BASEDIR/bootable/recovery
+patch -p1 -N -i$PATCHERDIR/recovery.patch
+
+echo -e "\n\e[0;31mPatching AV:\e[0;34m"
+cd $BASEDIR/frameworks/av
+patch -p1 -N -i$PATCHERDIR/av.patch
+
+echo -e "\n\e[0;31mPatching BASE:\e[0;34m"
+cd $BASEDIR/frameworks/base
+patch -p1 -N -i$PATCHERDIR/base.patch
+
+echo -e "\n\e[0;31mPatching NATIVE:\e[0;34m"
+cd $BASEDIR/frameworks/native
+patch -p1 -N -i$PATCHERDIR/native.patch
+
+echo -e "\n\e[0;31mPatching CORE:\e[0;34m"
+cd $BASEDIR/system/core
+patch -p1 -N -i$PATCHERDIR/core.patch
+
+echo -e "\n\e[0;31mPatching TELEPHONY:\e[0;34m"
+cd $BASEDIR/packages/services/Telephony
+patch -p1 -N -i$PATCHERDIR/telephony.patch
+
+echo -e "\n\e[0;32mEverything (probably) patched, have a nice day!\e[0m"
